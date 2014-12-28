c0a672b496d7dfab091bc9167c4ab3b590b9eb51
Blue <j71200@gmail.com>
2014-12-29 03:14:31 +0800

    Plot

--- src/blue/testproject3/ChartView.java
+++ src/blue/testproject3/ChartView.java
@@ -1,20 +1,29 @@
 package blue.testproject3;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 
 public class ChartView extends View {
 	
 	private Paint min_paint = new Paint();
-	
-	public ChartView(Context context) {
-		super(context);
-		// min_paint.setColor(orange);
-		// min_paint.setStrokeWidth(line_size);
-		// min_paint.setAlpha(180);
+	private ArrayList<Integer> cord_x = new ArrayList<Integer>();
+	private ArrayList<Integer> cord_y = new ArrayList<Integer>();
+		
+	public ChartView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		min_paint.setColor(Color.RED);
+		min_paint.setStrokeWidth(4);
+        
 	}
+	
 	
 	@Override
 	protected void onDraw(Canvas canvas) {
@@ -22,11 +31,22 @@ public class ChartView extends View {
 		drawLineChart(canvas);
 	}
 
-	private void drawLineChart(Canvas canvas) {
+	public void drawLineChart(Canvas canvas) {
+		if ( cord_x.size() == 0){
+	        cord_x.add(100);
+	        cord_y.add(400);
+			return;
+		}
 		
-		canvas.drawLine(0, 0, 1, 1, min_paint);
+		Random rand = new Random();
+		cord_x.add( cord_x.get(cord_x.size()-1) + 50 );
+        cord_y.add( 400 + rand.nextInt(200) - 100 );
+        
+		for(int idx = 0; idx < cord_x.size()-2; idx++){
+			canvas.drawLine(cord_x.get(idx), cord_y.get(idx), cord_x.get(idx+1), cord_y.get(idx+1), min_paint);
+		}
 		
+		Log.d("ChartView", "At_drawLineChart: " + cord_x.size());
 	}
-	
-
 }
+
--- src/blue/testproject3/MobilityFragment.java
+++ src/blue/testproject3/MobilityFragment.java
@@ -1,41 +1,66 @@
 package blue.testproject3;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
+import android.annotation.SuppressLint;
 import android.app.Fragment;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.FrameLayout;
-import android.widget.LinearLayout;
 import blue.testproject3.ChartView;
 
 
 public class MobilityFragment extends Fragment {
 	
 	private View view;
-	private ChartView lineChart;
+	private ChartView chartView;
+	
+	private Timer timerForPlot = new Timer();
+	private int elapse = 0;
+	
+    private TimerTask task = new TimerTask(){
+        @Override
+        public void run() {
+        	elapse++;
+        	if(elapse <= 20){
+        		Message message = new Message();
+                message.what = 1;
+                handler.sendMessage(message);
+        	}
+        	else{
+        		timerForPlot.cancel();
+        	}
+        }
+    };
+    
+    @SuppressLint("HandlerLeak")
+    private Handler handler = new Handler(){
+		 public void handleMessage(Message msg){
+			super.handleMessage(msg);
+			chartView.invalidate();
+		 }
+    };
+	
+	
+	
+	@Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        timerForPlot.scheduleAtFixedRate(task, 0, 300);
+        
+    }
+	
 	
 	@Override
 	public View onCreateView (LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
 		
-		FrameLayout layout=(FrameLayout) view.findViewById(R.id.canvas_frame);  
-        final ChartView lineChart = new ChartView(this);
-        lineChart.setMinimumHeight(500);
-        lineChart.setMinimumWidth(300);
-        //通知view组件重绘    
-        lineChart.invalidate();
-        layout.addView(lineChart);
+		view = inflater.inflate(R.layout.fragment_mobility, container, false);
+		chartView = (ChartView) view.findViewById(R.id.chartView);
 		
-//		// FrameLayout canvasFrameLayout = (FrameLayout) view.findViewById(R.id.canvas_frame);
-//        // final ChartView lineChart = new ChartView();
-//		lineChart = (ChartView) view.findViewById(R.id.chartView);
-////		if(lineChart == null)
-////			return view;
-//		
-//        //通知view组件重绘    
-//        lineChart.invalidate();
-//        // canvasFrameLayout.addView(lineChart); 
-        
-		return inflater.inflate(R.layout.fragment_mobility, container, false);
+		return view;
 	}
 }
